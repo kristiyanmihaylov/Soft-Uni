944bd3104d744ecb9a523b6381a6314c3a3642c7
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-07-30 18:54:59 +0200

    pregovor

--- Javascript/5. For cycle/Exercise/pregovor.js
+++ Javascript/5. For cycle/Exercise/pregovor.js
@@ -15,7 +15,12 @@ console.log(num); //8
 let perInfo = 'Hello, my name is Miro and I like JavaScript.';
 console.log(perInfo.length); // shows how many symbols are there
 console.log(perInfo[0]); // shows "H" b/c I must count from 0
-
+/*
+function histogram(input) {
+    let n = Number(input[0]);
+for(let i = 1; i <= n; i++ ) {
+    let number = Number(input[i]);
+    */
 // => there are not the same concepts
 // 1voto pokazva broikata simvoli a 0 ne e broika
 // no kogato broim izpolzvame 0 kato nachalo
