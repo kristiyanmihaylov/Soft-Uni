ba1a3c48f90ea45f5d245c1bc6a283ae9a6c7f55
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-08-22 15:27:45 +0200

    ingegrade

--- Javascript/7.Nested Loop/Lab/01. Clock-2.js
+++ Javascript/7.Nested Loop/Lab/01. Clock-2.js
@@ -1,6 +1,6 @@
 function clock() {
     for (let h = 0; h <= 23; h++) {
-        for (let m = 0; m <= 23; m++) {
+        for (let m = 0; m <= 59; m++) {
             console.log(`${h}:${m}`);
         }
     }
--- Javascript/7.Nested Loop/Lab/07. Cinema Tickets.js
+++ Javascript/7.Nested Loop/Lab/07. Cinema Tickets.js
@@ -1,66 +1,77 @@
-function cinemaTickets(input) {
+function solve(input) {
     let index = 0;
     let command = input[index];
     index++;
+    let studentTickets = 0;
     let standardTickets = 0;
     let kidTickets = 0;
-    let studentTickets = 0;
 
-    while (command !== 'Finish' && index < input.length) {
+    while (command !== "Finish" && index < input.length) {
         let name = command;
         let places = Number(input[index]);
         index++;
         let ticketType = input[index];
         index++;
         let ticketsBought = 0;
-        while (ticketType !== 'End') {
-            let type = ticketType
-            if (type === 'standard') {
-                standardTickets++;
-            } else if (type === 'kid') {
-                kidTickets++;
-            } else if (type === 'student') {
+
+        while (ticketType !== "End") {
+            let type = ticketType;
+
+            if (type == "student") {
                 studentTickets++;
+            } else if (type == "standard") {
+                standardTickets++;
+            } else if (type == "kid") {
+                kidTickets++;
             }
+
             ticketsBought++;
-            if (ticketsBought === places) {
+            if (ticketsBought == places) {
                 break;
             }
+
             ticketType = input[index];
             index++;
         }
+
         console.log(
             `${name} - ${((ticketsBought / places) * 100).toFixed(2)}% full.`
         );
+
         command = input[index];
         index++;
     }
+
     let total = studentTickets + standardTickets + kidTickets;
     console.log(`Total tickets: ${total}`);
     console.log(`${((studentTickets / total) * 100).toFixed(2)}% student tickets.`);
     console.log(`${((standardTickets / total) * 100).toFixed(2)}% standard tickets.`);
     console.log(`${((kidTickets / total) * 100).toFixed(2)}% kids tickets.`);
-
-
-
 }
-cinemaTickets([
-    "Taxi",
-    "10",
+solve(["The Matrix",
+    "20",
+    "student",
     "standard",
+    "kid",
     "kid",
     "student",
     "student",
     "standard",
-    "standard",
+    "student",
     "End",
-    "Scary Movie",
-    "6",
+    "The Green Mile",
+    "17",
     "student",
+    "standard",
+    "standard",
     "student",
+    "standard",
     "student",
-    "student",
-    "student",
-    "student",
+    "End",
+    "Amadeus",
+    "3",
+    "standard",
+    "standard",
+    "standard",
     "Finish"
 ]);
