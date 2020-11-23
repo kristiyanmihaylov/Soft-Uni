a3c2f90480c7778b43da04ec242eff34b9e925a1
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-11-23 23:06:08 +0200

    done another 45 min of lesson

---
+++ Javascript/2. Fundamentals/1. Basic Syntax/Basics.js
@@ -0,0 +1,29 @@
+function multiplyByTwo(num){
+    let result = num*2;
+    console.log(result);
+}
+// don't send the entry inputs
+multiplyByTwo(2.44);
+multiplyByTwo(1);
+multiplyByTwo(3.55);
+
+/////////////////////////////////////
+
+/*function solve(num1, num2){
+    if(num1 > num2){
+    console.log(Pesho);
+} else {
+    console.log(Kris);
+}
+}
+solve(2, 5);
+*/
+
+////////////////////////
+
+/*let a = 5;
+let b = 13;
+console.log(a+b);
+if(b>a){
+    console.log(b);
+}*/
