9b647b8e674958df5bbb243ecc26a82df7eb2556
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-11-24 22:57:38 +0200

    another 30 min into the lesson done

--- Javascript/2. Fundamentals/1. Basic Syntax/Basics.js
+++ Javascript/2. Fundamentals/1. Basic Syntax/Basics.js
@@ -1,12 +1,63 @@
-function multiplyByTwo(num){
+/*For Loops*/
+
+function num(num1){
+    for(num1=1;num1<=5;num1++){
+        console.log(num1);
+    }
+}
+num(1);
+
+/*Switch Function - use them not often at all*/
+/*
+function mag(fruit){
+    switch(fruit){
+        case "banana":
+            console.log('1.25');
+            break;
+        case 'apple':
+            console.log('1.33');
+            break;
+    }
+}
+mag('banana')
+*/
+
+/* IF ELSE FUNCTION */
+
+/*function grade(num){
+    if(num>=5.5){
+        console.log('Excellent');
+    } else if(num<3){
+        console.log('Not excellent');
+    } else {
+        console.log('Failed);
+    }
+}
+grade(4.3);
+*/
+
+/*function grade(grade1, grade2){
+    if(grade1>grade2){
+        console.log('Excellent');
+    } else{
+        console.log('Not excellent');
+    }
+}
+grade(5.5, 4.35)
+*/
+
+/*MATH FUNCTIONS*/
+
+/*function multiplyByTwo(num){
     let result = num*2;
     console.log(result);
 }
 // don't send the entry inputs
+// code is free = there is enough memory - writing more is sometimes better than writing less Variables
 multiplyByTwo(2.44);
 multiplyByTwo(1);
 multiplyByTwo(3.55);
-
+*/
 /////////////////////////////////////
 
 /*function solve(num1, num2){
