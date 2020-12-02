64f73d541f5ba0c03985301db00e83620c235efb
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-12-02 22:05:21 +0200

    masiv remind

---
+++ Javascript/2. Fundamentals/1. Basic Syntax/exercise/masiv.js
@@ -0,0 +1,25 @@
+/* FIND IN MASIV INDEX
+
+function test(arr) {
+    console.log(arr[arr.length - 1]);
+}
+test(['1', '2', '3']);
+
+//3
+*/
+
+/* MASIV
+
+function test(a) {
+    console.log(a[0]);
+    //purvi masiv
+    console.log(a[1]);
+    //vtori masiv
+    console.log(a[2]);
+    //treti masiv
+}
+test(['1', '2', '3']); */
+
+//1
+//2
+//3
