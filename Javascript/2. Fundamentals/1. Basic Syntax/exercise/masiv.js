0438b85306aea20d27dc08fe67436d14272d83d5
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-12-06 12:04:37 +0200

    continue

--- Javascript/2. Fundamentals/1. Basic Syntax/exercise/masiv.js
+++ Javascript/2. Fundamentals/1. Basic Syntax/exercise/masiv.js
@@ -1,3 +1,11 @@
+function test(arr) {
+    let n = arr.shift(); // izvajda '1' ot masiva
+    console.log(n);
+    console.log(arr);
+}
+test(['1', '2', '3']);
+
+
 /* FIND IN MASIV INDEX
 
 function test(arr) {
@@ -13,7 +21,7 @@ test(['1', '2', '3']);
 function test(a) {
     console.log(a[0]);
     //purvi masiv
-    console.log(a[1]);
+    console.log(a[1]); 
     //vtori masiv
     console.log(a[2]);
     //treti masiv
