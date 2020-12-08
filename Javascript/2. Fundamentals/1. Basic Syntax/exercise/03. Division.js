ac804e2148fff465da5b49e8a2b1b74bc89703f0
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-12-08 23:33:34 +0200

    1;47;00

---
+++ Javascript/2. Fundamentals/1. Basic Syntax/exercise/03. Division.js
@@ -0,0 +1,20 @@
+ function division(num) {
+     if (num % 10 == 0) { //Modulno delenie
+         console.log('The number is divisible by 10');
+     } else if (num % 7 == 0) {
+         console.log('The number is divisible by 7');
+     } else if (num % 6 == 0) {
+         console.log('The number is divisible by 6');
+     } else if (num % 3 == 0) {
+         console.log('The number is divisible by 3');
+     } else if (num % 2 == 0) {
+         console.log('The number is divisible by 2');
+     } else {
+         console.log('Not divisible');
+     }
+ }
+ division(12);
+
+ //2,3,6,7,10
+
+ //  Modulno Delenie
