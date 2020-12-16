3d17d9b854897a9be1ab9b3c45ba6edca53caff0
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-12-16 21:50:32 +0200

    20;00

---
+++ Javascript/2. Fundamentals/2. Data Types & Variables/lab/basics.js
@@ -0,0 +1,25 @@
+let myVar = 5;
+
+function myFunc() {
+    // inside scope
+    console.log('inside function');
+    console.log(myVar);
+
+    if (myVar == 5) {
+        var result = 'some result'; //var ako iskam da go
+        // polzvam izvun if{} cikula
+    }
+
+    /*
+    for (var i = 0; i < 10; i++) {
+        console.log(i);
+    } //using var I can get the loop global
+    */
+    console.log('outside loop');
+    console.log(result);
+}
+//global scope
+console.log('global scope');
+console.log(myVar);
+
+myFunc();
---
+++ Javascript/2. Fundamentals/2. Data Types & Variables/lab/basics2.js
@@ -0,0 +1,26 @@
+var someGlobal = 5;
+
+function myFunc() {
+    for (let i = 0; i < 10; i++) {
+        // 
+    }
+}
+myFunc();
+
+
+/*
+var someGlobal = 5; 
+// vs dr filelove q vijdat
+// zashtoto se zakacha za globalniq file
+
+function myFunc() {
+    console.log(myVar);
+
+    var myVar = 'peter';
+}
+myFunc();
+
+// undefinded
+// zashtoto trqbva vinagi da e predi da
+// q zastupim kato output
+*/
