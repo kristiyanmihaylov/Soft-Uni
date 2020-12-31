3201281c52eb7382a2201a30ca60004e18924498
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-12-31 17:11:13 +0200

    http basics

---
+++ Javascript/2. Fundamentals/3. Http/httpBasics.js
@@ -0,0 +1,28 @@
+/*
+
+HTTP
+HTTPS
+Protocol
+IP
+
+Chrome Dev Tools F12
+
+HTML Forms 1;35;00
+
+HTTP Reqiests
+// get - retrieve recource
+// posit - create/store recource
+-- put info in a form/
+// delete - delete recource
+-- iztrivame informaciqta
+// patch - modify recource
+-- chastichno promenqne (user,pass,phone = change only phone)
+// put - update recource
+-- promenq informaciqta v daden tab
+// head - retrieve recourse headers
+-- vrushta headers of recource but not the recourse itself 
+-- when was changed
+
+CRUD = Create Retrieve Update Delete
+
+*/
