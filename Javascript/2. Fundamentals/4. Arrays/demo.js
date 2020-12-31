875c46bc721e6aef9c7ddf18b6c7899a28f085f1
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-12-31 18:30:06 +0200

    35;00

---
+++ Javascript/2. Fundamentals/4. Arrays/demo.js
@@ -0,0 +1,27 @@
+/*
+
+Arrays - Sequences of Elements
+
+4. For-of loops
+
+3. Array teration
+
+2. Operations
+
+1. Definition
+
+Elements are numbered from 0 to length-1
+
+0=1
+1 index =2
+2=3
+3=4
+4=5th element
+
+Dava ni funkciq koqto mojem da postvame info
+i to da bude zahraneno kym celiq masiv
+
+nqma znachenie dali shte ima 3 ili 300 danni
+koito imat ednakva structura na edno mqsto
+
+*/
