937177b5dc65d4a222a88630960dbfb0bcaf28cd
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2021-01-02 19:34:10 +0200

    finished lab

--- Javascript/2. Fundamentals/4. Arrays/04. Reverse an Array of Strings.js
+++ Javascript/2. Fundamentals/4. Arrays/04. Reverse an Array of Strings.js
@@ -1,4 +1,26 @@
-function switchArr(words) {
+function swap(arr) {
 
+    let lastIndex = arr.length - 1;
+    // zapazvame stoinostta v edna promenliva
+
+    // vzimame stoinostta ot masiva
+    // razmenqmae stoinostta s ogledalnata
+    for (let i = 0; i < arr.length / 2; i++) {
+        //taka minava samo do polovinata i prekusva predi sredata
+
+        // zapazvame stoinostta v stranichna promenliva
+        let temp = arr[i];
+        // namirame razstoqnieto do simetralata
+        //i = distance from start
+        //length-1-i = distance from end
+
+        // opredelqme ogledaniq index
+        // zapisvame stoinostta na ogledalniq 
+        arr[i] = arr[lastIndex - i];
+
+        // zapisvame stoinostta ot vunshnata na mqstoto na ogledalniq
+        arr[lastIndex - i] = temp
+    }
+    console.log(arr.join(' '));
 }
-switchArr(['a', 'b', 'c', 'd', 'e']);
+swap(['a', 'b', 'c', 'd', 'e']);
